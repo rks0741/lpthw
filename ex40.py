f8f39953130f1052f36391ca1fc0443e850e5fab
rks0741 <71434476+rks0741@users.noreply.github.com>
2020-12-15 10:25:07 +0300

    update 40
    40 update

--- ex40.py
+++ ex40.py
@@ -8,7 +8,17 @@ class Song(object):
             print(line)
 
 happy_bday = Song(["Happy bday to you", "I do not want to get sued", "So i will stop right here"])
-bulls_parade = Song(["Happy bday to you", "I do not want to get sued", "So i will stop right here"])
+bulls_parade = Song(["They rally around tha family", "With pockets full of shells"])
+
+katusha = Song(["Vihodila na bereg katusha", "Na visokii beereg na krutoi", "Tam h3 chto dalshe"])
+katusha_lyrics = ["Vihodila na bereg katusha2", "Na visokii beereg na krutoi2", "Tam h3 chto dalshe2"]
+
 
 happy_bday.sing_me_a_song()
 bulls_parade.sing_me_a_song()
+
+katusha.sing_me_a_song()
+
+katusha_l = Song(katusha_lyrics)
+
+katusha_l.sing_me_a_song()
